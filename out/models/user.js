ccd9e9a5907bdb475a911ffffd08ba21489cc904
Awaleed <a.waleed0011@gmail.com>
2020-05-10 09:07:04 +0200

    new start

--- out/models/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var mongoose = require('mongoose');
-var passportLocalMongoose = require('passport-local-mongoose');
-var userSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', userSchema);
