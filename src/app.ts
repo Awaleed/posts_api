0ddfb5270b10249dbe6bfa6eca093fb3824a41e7
Awaleed <a.waleed0011@gmail.com>
2020-05-10 15:09:47 +0200

    updated postes with validation and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,49 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import Controller from "interfaces/controller.interface";
+import errorMiddleware from "./middleware/error.middleware";
 
 class App {
   public app: express.Application;
-  public port: number;
 
-  constructor(controllers, port) {
+  constructor(controllers: Controller[]) {
     this.app = express();
-    this.port = port;
 
+    this.connectToTheDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
+    this.initializeErrorHandling();
   }
 
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
   }
 
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+
   private initializeControllers(controllers) {
-    controllers.forEach((controller) => {
+    controllers.forEach((controller: Controller) => {
       this.app.use("/", controller.router);
     });
   }
 
+  private connectToTheDatabase() {
+    const { MONGO_PATH } = process.env;
+    mongoose.connect(
+      MONGO_PATH,
+      { useUnifiedTopology: true, useNewUrlParser: true },
+      (err) => {
+        if (err) console.error(`DB: error ${err}`);
+        else console.log(`DB: connected`);
+      }
+    );
+  }
   public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App listening on the port ${process.env.PORT}`);
     });
   }
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,35 +1,102 @@
 import * as express from "express";
+import Controller from "../interfaces/controller.interface";
 import Post from "./post.interface";
+import postModel from "./posts.model";
+import PostNotFoundException from "../exceptions/PostNotFoundException";
+import validationMiddleware from "../middleware/validation.middleware";
+import CreatePostDto from "./post.dto";
 
-class PostsController {
+class PostsController implements Controller {
   public path = "/posts";
   public router = express.Router();
-
-  private posts: Post[] = [
-    {
-      author: "Marcin",
-      content: "Dolor sit amet",
-      title: "Lorem Ipsum",
-    },
-  ];
+  private post = postModel;
 
   constructor() {
     this.initializeRoutes();
   }
 
-  public initializeRoutes() {
+  private initializeRoutes() {
     this.router.get(this.path, this.getAllPosts);
-    this.router.post(this.path, this.createAPost);
+    this.router.get(`${this.path}/:id`, this.getPostById);
+    this.router.patch(
+      `${this.path}/:id`,
+      validationMiddleware(CreatePostDto, true),
+      this.modifyPost
+    );
+    this.router.delete(`${this.path}/:id`, this.deletePost);
+    this.router.post(
+      this.path,
+      validationMiddleware(CreatePostDto),
+      this.createPost
+    );
   }
 
-  getAllPosts = (request: express.Request, response: express.Response) => {
-    response.send(this.posts);
+  private getAllPosts = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    this.post.find().then((posts) => {
+      response.send(posts);
+    });
   };
 
-  createAPost = (request: express.Request, response: express.Response) => {
-    const post: Post = request.body;
-    this.posts.push(post);
-    response.send(post);
+  private getPostById = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    this.post.findById(id, (err: any, post: Post) => {
+      if (err) {
+        return next(new PostNotFoundException(id));
+      }
+      response.send(post);
+    });
+  };
+
+  private modifyPost = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    const postData: Post = request.body;
+    this.post.findByIdAndUpdate(
+      id,
+      postData,
+      { new: true },
+      (err: any, post: Post) => {
+        if (err) {
+          return next(new PostNotFoundException(id));
+        }
+        response.send(post);
+      }
+    );
+  };
+
+  private createPost = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const postData: Post = request.body;
+    const createdPost = new this.post(postData);
+    createdPost.save().then((savedPost) => {
+      response.send(savedPost);
+    });
+  };
+
+  private deletePost = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    this.post.findByIdAndDelete(id, (err: any, post: Post) => {
+      if (err) {
+        return next(new PostNotFoundException(id));
+      }
+      response.send(post);
+    });
   };
 }
 
