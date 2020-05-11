bcb434dab99ae8dcdfd7daf3b38a36a0d29ca4b2
Awaleed <a.waleed0011@gmail.com>
2020-05-11 13:31:26 +0200

    finshed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import mongoose from "mongoose";
 import Controller from "interfaces/controller.interface";
 import errorMiddleware from "./middleware/error.middleware";
+import loggerMiddleware from "./middleware/logger.middleware";
 
 class App {
   public app: express.Application;
@@ -37,12 +38,21 @@ class App {
   }
 
   private initializeControllers(controllers: Controller[]) {
+    this.app.use(express.static(path.join(__dirname, "public")));
+    this.app.set("views", path.join(__dirname, "./"));
+    this.app.set("view engine", "ejs");
+
+    this.app.get("/", function (req, res) {
+      res.render("public/landing", { title: "YelpCamp" });
+    });
+
     controllers.forEach((controller) => {
       this.app.use("/", controller.router);
     });
   }
 
   private initializeErrorHandling() {
+    this.app.use(loggerMiddleware);
     this.app.use(errorMiddleware);
   }
 
--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema(
     lastName: String,
     password: {
       type: String,
+      get: (): undefined => undefined,
     },
   },
   {
